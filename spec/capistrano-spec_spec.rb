1b16bf1757805d665e78c2e7ffcdb4327dfcbd43
petems <p.morsou@gmail.com>
2013-03-27 23:48:17 +0000

    First run at some basic sanity checks

--- spec/capistrano-spec_spec.rb
+++ spec/capistrano-spec_spec.rb
@@ -1,7 +1,42 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Capistrano::Spec do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+
+  require 'capistrano'
+
+  module Capistrano
+    module Foo
+      def self.load_into(configuration)
+        configuration.load do
+          set :bar, "baz"
+        end
+      end
+    end
   end
+
+  if Capistrano::Configuration.instance
+    Capistrano::Foo.load_into(Capistrano::Configuration.instance)
+  end
+
+  before do
+    @configuration = Capistrano::Configuration.new
+    @configuration.extend(Capistrano::Spec::ConfigurationExtension)
+    @configuration.extend(Capistrano::Foo)
+    Capistrano::Foo.load_into(@configuration)
+  end
+
+  describe Capistrano::Spec::Matchers do
+    it "should have a #callback matcher" do
+      expect{@configuration.should callback("fake_task")}.to_not raise_error(NoMethodError)
+    end
+
+    it "should have a #have_uploaded matcher" do
+      expect{@configuration.should have_uploaded("fake_upload")}.to_not raise_error(NoMethodError)
+    end
+
+    it "should have a #have_run matcher" do
+      expect{@configuration.should have_run("fake_task")}.to_not raise_error(NoMethodError)
+    end
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,14 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'capistrano'
 require 'capistrano-spec'
 require 'rspec'
 require 'rspec/autorun'
 
 RSpec.configure do |config|
-
+	config.color_enabled = true
+  	config.tty = true
+  	config.formatter = :documentation
+	config.include Capistrano::Spec::Matchers
+	config.include Capistrano::Spec::Helpers
 end
